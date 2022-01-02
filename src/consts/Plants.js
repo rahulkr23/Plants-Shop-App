08afff74bfc6159b5a30ce78e35acd5b5bf3574d
rahulkr23 <rahulwhy23@gmail.com>
2022-01-03 04:02:17 +0530

    home screen item added done

---
+++ src/consts/Plants.js
@@ -0,0 +1,93 @@
+const plants = [
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+    {
+        id: 1,
+        name: 'Succulent Plant',
+        price: '39.99',
+        like: true,
+        img: require('../assets/1.jpg'),
+        about:
+            'scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into'
+    },
+]
+export default plants;
